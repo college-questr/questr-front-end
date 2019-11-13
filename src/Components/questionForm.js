5b5416c9792befe4c0d2f02c26d1ee5a592498ad
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-11-13 12:10:12 -0800

    Basic form

---
+++ src/Components/questionForm.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const QuestionForm = () => {
+
+
+  return (
+    <form>
+      <h1>Ask your Questions here</h1>
+      <input
+        type="text"
+        name="question"
+        placeholder="Ask your question"
+      />
+    </form>
+  )
+
+}
+
+export default QuestionForm;
---
+++ src/Route/questionFormRoute.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import QuestionForm from '../Components/questionForm'
+
+const QuestionFormPage = (props) => {
+  return (
+    <div className='container'>
+      <QuestionForm />
+    </div>
+  )
+}
+
+export default QuestionFormPage;
