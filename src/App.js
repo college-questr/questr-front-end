04d639b6e19c7c4ff55d093ba264f595d8d3e8f7
DJ Coleman <DJ@DJs-MacBook-Pro.local>
2019-11-13 21:00:13 -0800

    Added questionItem

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route } from 'react-router-dom';
 import LoginRoute from './Route/loginRoute';
 import SignupRoute from './Route/signupRoute';
 import QuestionFormRoute from './Route/questionFormRoute';
+import DashboardRoute from './Route/dashboardRoute';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Route exact path='/login' component={LoginRoute} />
       <Route exact path='/signup' component={SignupRoute} />
       <Route exact path='/add-question' component={QuestionFormRoute} />
+      <Route exact path='/dashboard' component={DashboardRoute} />
     </div>
   );
 }
