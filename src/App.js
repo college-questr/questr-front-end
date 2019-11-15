66f310389d1e8a97bdf9c7603586ca6bd275599f
kiamco <kiamcoo@gmail.com>
2019-11-14 22:53:44 -0800

    clean tests

--- src/App.js
+++ src/App.js
@@ -5,8 +5,6 @@ import LoginRoute from './Route/loginRoute';
 import SignupRoute from './Route/signupRoute';
 import QuestionFormRoute from './Route/questionFormRoute';
 import DashboardRoute from './Route/dashboardRoute';
-import QuestionDetail from './Components/QuestionDetails/questionDetail'
-import {questionDetailObj} from './Components/testComponentData'
 
 function App() {
   return (
@@ -15,7 +13,6 @@ function App() {
       <Route exact path='/signup' component={SignupRoute} />
       <Route exact path='/add-question' component={QuestionFormRoute} />
       <Route exact path='/dashboard' component={DashboardRoute} />
-      <QuestionDetail questionDetails={questionDetailObj} />
     </div>
   );
 }
