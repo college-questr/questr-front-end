058d0a26cbab6f400d6bf51435f2200073d2134e
kiamco <kiamcoo@gmail.com>
2019-11-15 12:53:08 -0800

    split questionDetails in to two components

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import LoginRoute from './Route/loginRoute';
 import SignupRoute from './Route/signupRoute';
 import QuestionFormRoute from './Route/questionFormRoute';
 import DashboardRoute from './Route/dashboardRoute';
+import QuestionDetail from './Components/QuestionDetails/questionDetail'
+import {questionDetailObj} from './Components/testComponentData'
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
       <Route exact path='/signup' component={SignupRoute} />
       <Route exact path='/add-question' component={QuestionFormRoute} />
       <Route exact path='/dashboard' component={DashboardRoute} />
+      <QuestionDetail questionDetails={questionDetailObj} />
     </div>
   );
 }
---
+++ src/Components/QuestionDetails/answers.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Style from 'styled-components';
+import {
+    MessageBody,
+    Stamp,
+    Vote,
+    Arrow
+} from './sharedStyles'
+import Down from '../../Img/downArr.png';
+import Up from '../../Img/upArrow.png';
+
+const Answers = ({answers}) => {
+
+    return (
+        <>
+            <AnswerHeader>{answers.length} Answers</AnswerHeader>
+            <AnswerContainer>
+                {answers.map(el => {
+                    return (
+                        <SingleAnswer>
+                            <Vote>
+                                <Arrow src={Up} />
+                                {el.votes}
+                                <Arrow src={Down} />
+                            </Vote>
+                            <MessageBody>
+                                {el.answer}
+                                <Stamp>
+                                    Answered by {el.answeredBy}
+                                    <br />
+                                    {el.createdAt}
+                                </Stamp>
+                            </MessageBody>
+                        </SingleAnswer>
+                    )
+                })}
+            </AnswerContainer>
+
+        </>
+    )
+}
+
+
+export default Answers;
+
+const AnswerContainer = Style.div`
+    display:flex;
+    flex-flow:row wrap;
+
+ `
+
+const SingleAnswer = Style.div`
+    display: flex;
+    flew-flow: row wrap;
+    border-bottom: 1px solid #D3D0D0;
+
+ `
+
+const AnswerHeader = Style.h1`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: normal;
+    font-size: 36px;
+    line-height: 42px;
+    border-bottom: 1px solid #D3D0D0;
+    color: rgba(0, 0, 0, 0.54);
+`
