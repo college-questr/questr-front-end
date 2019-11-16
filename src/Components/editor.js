cb57d15cb3171b73dc7924e74ede4223660bbb39
kiamco <kiamcoo@gmail.com>
2019-11-16 14:14:06 -0800

    addded submit button for editor component

--- src/Components/editor.js
+++ src/Components/editor.js
@@ -1,54 +1,79 @@
 import React from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import Style from 'styled-components';
 
 class Editor extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            text: "",
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "",
+    }
+  }
+
+  modules = {
+    toolbar: [
+      [{ 'header': [1, 2, false] }],
+      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+      ['link', 'image'],
+      ['clean']
+    ],
+  };
+
+  formats = [
+    'header',
+    'bold', 'italic', 'underline', 'strike', 'blockquote',
+    'list', 'bullet', 'indent',
+    'link', 'image'
+  ];
+
+   handleChange = (value) => {
+      this.setState({
+        text: value
+      })
+
+      console.log(this.state.text)
     }
 
-    modules = {
-        toolbar: [
-            [{ 'header': [1, 2, false] }],
-            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-            ['link', 'image'],
-            ['clean']
-        ],
-    };
-
-    formats = [
-        'header',
-        'bold', 'italic', 'underline', 'strike', 'blockquote',
-        'list', 'bullet', 'indent',
-        'link', 'image'
-    ];
-
-    handleChange = (value) => {
-        this.setState({
-            text: value
-        })
-
-        console.log(this.state.text)
+    submit = (e) => {
+      e.preventDefault();
+      //send parsed state text to api
     }
 
 
-    render() {
-        return ( <
-            div className = "text-editor" >
-            <
-            ReactQuill theme = "snow"
-            modules = { this.modules }
-            formats = { this.formats }
-            onChange = { this.handleChange } >
-            <
-            /ReactQuill> <
-            /div>
-        );
-    }
+  render() {
+    return (
+      <div className="text-editor">
+        <ReactQuill theme="snow"
+          modules={this.modules}
+          formats={this.formats}
+          onChange={this.handleChange}
+        >
+        </ReactQuill>
+
+        <PostSubmitBtn>
+          Follow
+        </PostSubmitBtn>
+      </div>
+    );
+  }
 }
 
 export default Editor;
+
+const PostSubmitBtn = Style.button`
+  width: 311px;
+  height: 37px;
+  background: #93B2E0;
+  border-radius: 3px;
+  color:white;
+  transition:500ms;
+  text-decoration:none
+
+  &:hover{
+      border: 5px solid #93B2E0;
+      cursor:pointer;
+
+  }
+`
