da141c8d46c939a262a8204f993d3f23d62915c1
kiamco <kiamcoo@gmail.com>
2019-11-16 10:54:30 -0800

    added handle change for quill editor

--- src/Components/editor.js
+++ src/Components/editor.js
@@ -3,40 +3,52 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 class Editor extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      text: "",
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: "",
+        }
     }
-  }
 
-  modules = {
-    toolbar: [
-      [{ 'header': [1, 2, false] }],
-      ['bold', 'italic', 'underline', 'strike', 'blockquote'],
-      [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
-      ['link', 'image'],
-      ['clean']
-    ],
-  };
+    modules = {
+        toolbar: [
+            [{ 'header': [1, 2, false] }],
+            ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+            [{ 'list': 'ordered' }, { 'list': 'bullet' }, { 'indent': '-1' }, { 'indent': '+1' }],
+            ['link', 'image'],
+            ['clean']
+        ],
+    };
 
-  formats = [
-    'header',
-    'bold', 'italic', 'underline', 'strike', 'blockquote',
-    'list', 'bullet', 'indent',
-    'link', 'image'
-  ];
+    formats = [
+        'header',
+        'bold', 'italic', 'underline', 'strike', 'blockquote',
+        'list', 'bullet', 'indent',
+        'link', 'image'
+    ];
 
-  render() {
-    return (
-      <div className="text-editor">
-        <ReactQuill theme="snow"
-          modules={this.modules}
-          formats={this.formats}>
-        </ReactQuill>
-      </div>
-    );
-  }
+    handleChange = (value) => {
+        this.setState({
+            text: value
+        })
+
+        console.log(this.state.text)
+    }
+
+
+    render() {
+        return ( <
+            div className = "text-editor" >
+            <
+            ReactQuill theme = "snow"
+            modules = { this.modules }
+            formats = { this.formats }
+            onChange = { this.handleChange } >
+            <
+            /ReactQuill> <
+            /div>
+        );
+    }
 }
 
 export default Editor;
--- src/Route/questionDetailRoute.js
+++ src/Route/questionDetailRoute.js
@@ -1,9 +1,32 @@
 import React from 'react'
 import QuestionDetail from '../Components/QuestionDetails/questionDetail';
+import Editor from '../Components/editor';
+import Subscribe from '../Components/subscribe';
+
+import {
+  questionDetailObj
+} from '../Components/testComponentData'
 
 const QuestionDetailPage = (props) => {
   return (
-    <QuestionDetail />
+    <>
+      <div className='question-detail-contianer'>
+        <div className='left-detail'>
+        < QuestionDetail questionDetails={questionDetailObj} />
+        <div className="header-container">
+            <h1>Post Your Answer </h1>
+            <p>Before you post your answer, please take a moment to go through our tips on great answers.</p>
+        </div>
+        <div className="editor">
+            <Editor />
+        </div>
+        </div>
+        <div className="right-detail">
+          <Subscribe followers={questionDetailObj.followers} />
+
+        </div>
+      </div>
+    </>
   )
 }
 
