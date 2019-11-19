ff10c4c655309a637923327918b0c6f82c4e7f1d
kiamco <kiamcoo@gmail.com>
2019-11-18 16:37:44 -0800

    added working gql queries

--- src/graphQL/queries.js
+++ src/graphQL/queries.js
@@ -1,11 +1,53 @@
 import { gql } from 'apollo-boost';
 
 
-export const GET_QUESTION_BY_ID = (id) => gql`
+export const GET_QUESTION_BY_ID_WITH_ANSWERS = (id) => gql`
     {
         question(id: "${id}") {
             questionTitle
             questionBody
+        	votes
+        	createdBy
+        	createdAt
+        	answer{
+                id
+                user_id
+                answer
+                votes
+                createdAt
+              }
+			tag{
+              question_id
+              tag
+            }
+        }
+    }
+`
+
+export const GET_QUESTION_BY_ID = (id) => gql`
+        {
+        question(id: "${id}") {
+            questionTitle
+            questionBody
+        	votes
+        	createdBy
+        	createdAt
+        }
+    }
+`
+
+export const GET_QUESTION_BY_ID_WITH_ANSWERS = (id) => gql`
+    {
+        question(id: "${id}") {
+            questionTitle
+            questionBody
+        	votes
+        	createdBy
+        	createdAt
+			tag{
+              question_id
+              tag
+            }
         }
     }
 `
