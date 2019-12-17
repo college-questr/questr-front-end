e00c95a11c10a32c8b6d8617a5350aa2fbafacdb
kiamco <kiamcoo@gmail.com>
2019-12-16 17:24:41 -0800

    add helemt

--- src/App.js
+++ src/App.js
@@ -7,11 +7,20 @@ import QuestionFormRoute from './Route/questionFormRoute';
 import DashboardRoute from './Route/dashboardRoute';
 import QuestionDetailPage from './Route/questionDetailRoute';
 import Search from './Route/searchRoute';
+import Helmet from 'react-helmet';
 
 
 function App(props) {
   
   return (
+    <>
+    <Helmet>
+      <title>Questr</title>
+      <meta name="description" content="Questr, an Open Source platform giving student a place they can ask questions related to their classes
+      and professors. Which allow them to build communities that's good enough to support themselves and other
+      individuals who don't have tutors or friends to study with." />
+      <meta name="keywords" content="react,seo,helmet" />   
+    </Helmet>
     <div className="App">
       <Route exact path='/login' component={LoginRoute} />
       <Route exact path='/signup' component={SignupRoute} />
@@ -20,6 +29,7 @@ function App(props) {
       <Route exact path='/question-detail/:id' component={QuestionDetailPage} />
       <Route exact path='/search=:searchKey' render={(props) => <Search {...props} />} />
     </div>
+    </>
   );
 }
 
